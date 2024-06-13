44d4609ebef8957209d35e7675c9a645ccf8ce4d
Muscardina <vergonzalezsainz@gmail.com>
2024-06-13 12:27:32 +0200

    Añadidas funcionalidades extras a ejercicio 1 y dos

--- 01-ascii.js
+++ 01-ascii.js
@@ -1,3 +1,13 @@
-let numero = 65;
-let caracter = String.fromCharCode(numero);
-console.log(caracter);  // Output: A
+let numero = (68, 55, 7);
+for (i = 1; i <= numero.length; i++) {
+  let caracter = String.fromCharCode(numero);
+  console.log(caracter); // Output: A
+}
+
+let caracter2 = "Marramiau";
+for (i = 1; i <= caracter2.length; i++) {
+
+  let numero2 = caracter2.charCodeAt(i - 1);
+
+  console.log(numero2);
+}
--- 02-ascii2utf8.js
+++ 02-ascii2utf8.js
@@ -2,4 +2,4 @@ function asciiAUtf8(caracter) {
     let codigoAscii = caracter.charCodeAt(0);
     let utf8Hex = codigoAscii.toString(16);
     return utf8Hex;
-}
+}console.log(asciiAUtf8("miau"))
